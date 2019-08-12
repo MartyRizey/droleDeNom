468ffc3da40172a6eb3aa1a3345e6a6d8c0e0d9e
MartyRizey <martyrizey@gmail.com>
2019-08-12 19:59:21 +0200

    Problème avec CSS page accueil

---
+++ v1.2-pageAccueil/app/views/accueil.php
@@ -0,0 +1,30 @@
+<?php
+    // print_r(__DIR__); die();
+    require __DIR__.'/templates/doctype-tpl.php';
+    require __DIR__.'/templates/header-tpl.php';
+?>
+
+<h1 class="accueil_title">Ici c'est la page accueil</h1>
+<!-- mettre fil d'ariane -->
+
+<a href="https://www.viedemerde.fr/article/les-20-noms-de-villes-francaises-les-plus-droles-mais-qu-en-pensent-les-habitants_230915.html"><h2 class="accueil_subTitle">Noms de villes marrant...</h2></a>
+
+<div>
+    <section class="article article_1 row">
+        <p class="col-xl-9"> accusamus quidem, ducimus adipisci amet facilis molestiae illum! Assumenda a necessitatibus hic velit, corporis nemo nulla doloremque officiis fuga voluptas alias nam amet cumque aliquid commodi rem ex iusto officia magni odit fugiat voluptates soluta voluptatibus. Officiis quis iure, adipisci, libero dolorum minima alias nam excepturi perspiciatis tenetur ab sapiente quaerat quod fugi at nulla!</p>
+        <img src="<?= IMG1ACCUEIL ?>" alt="panneau nom de la ville" class="photoArticle1 col-xl-3">
+    </section>
+</div>
+<hr>
+<div>
+    <section class="article article_2 row">
+        <img src="<?= IMG2ACCUEIL ?>" alt="panneau nom de la ville" class="photoArticle2">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus exercitationem nostrum maiores, necessitatibus consequatur consectetur quo nobis atque aliquam dolor id, minima eligendi ratione voluptatum adipisci a recusandae nisi quia consequuntur. Minus aperiam quas neque! At libero quod quos modi quibusdam rem officia amet? Earum aliquid perspiciatis nemo odio porro quasi cumque repellendus molestiae. Neque asperiores dolorem, consectetur beatae exercitationem tempora nobis hic quaerat aspernatur incidunt, distinctio iusto eius doloremque nisi molestiae obcaecati sit adipisci consequuntur! Ab optio ad, nulla voluptatibus ex tempore deserunt minus. Quasi aliquam repellendus quisquam quam saepe dolore quae veniam laborum veritatis quod in, recusandae voluptas debitis. Quasi adipisci harum inventore dolorem. Quae repudiandae perspiciatis obcaecati at ullam
+        corporis numquam, sequi nisi consectetur quidem nostrum voluptate dolor praesentium vero aut voluptas? Qui, numquam quas repellat velit nostrum voluptatibus, ipsum sit accusamus aliquam amet consequuntur soluta quos dolore quae recusandae eum dignissimos. Quidem possimus illum blanditiis maiores.</p>  
+    </section>
+</div>
+
+
+<?php
+    require __DIR__.'/templates/footer-tpl.php';
+?>
